c2920b9b0bfb123425b8aa6202474f96e88116bf
bogoevski-s <slave.bogoevski@yahoo.com>
2023-06-21 23:36:25 +0200

    class 8 added

---
+++ Cas-8/Slot-Machine/Slot-Machine/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Slot_Machine
+{
+    partial class SlotMachine
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SlotMachine));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(77, 94);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(106, 122);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(278, 94);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(106, 122);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(486, 94);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(106, 122);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox4.BackgroundImage")));
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox4.Location = new System.Drawing.Point(77, 236);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(509, 185);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(619, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 168);
+            this.button1.TabIndex = 4;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(619, 253);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 168);
+            this.button2.TabIndex = 5;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Verdana", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(776, 46);
+            this.label1.TabIndex = 6;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // SlotMachine
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SlotMachine";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Cas-8/Slot-Machine/Slot-Machine/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Slot_Machine
+{
+    public partial class SlotMachine : Form
+    {
+        public int Counter { get; set; } = 30;
+        public int DisplayOne { get; set; }
+        public int DisplayTwo { get; set; }
+        public int DisplayThree { get; set; }
+
+        public SlotMachine()
+        {
+            InitializeComponent();
+
+            label1.Text = "Пробајте си ја среќата!";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //flip na racka 1 nacin
+            //button1.Location = new Point(619, 253);
+
+            //var slika = new Bitmap(button1.BackgroundImage, new Size(button1.BackgroundImage.Width, button1.BackgroundImage.Height));
+
+            //slika.RotateFlip(RotateFlipType.Rotate180FlipX);
+
+            //button1.BackgroundImage = slika;
+
+            button1.Visible = false;
+            button2.Visible = true;
+            button2.Enabled = false;
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Counter--;
+            label1.Text = "......";
+            var random = new Random();
+
+            if(Counter > 0)
+            {
+                DisplayOne = random.Next(1, 4);
+                DisplayTwo = random.Next(1, 4);
+                DisplayThree = random.Next(1, 4);
+
+                switch (DisplayOne)
+                {
+                    case 1:
+                        pictureBox1.Image = Image.FromFile(@"..\..\..\..\..\images\2_apple.jpg");
+                        break;
+                    case 2:
+                        pictureBox1.Image = Image.FromFile(@"..\..\..\..\..\images\3_cherry.png");
+                        break;
+                    case 3:
+                        pictureBox1.Image = Image.FromFile(@"..\..\..\..\..\images\4_grapes.jpg");
+                        break;
+                }
+
+                switch (DisplayTwo)
+                {
+                    case 1:
+                        pictureBox2.Image = Image.FromFile(@"..\..\..\..\..\images\2_apple.jpg");
+                        break;
+                    case 2:
+                        pictureBox2.Image = Image.FromFile(@"..\..\..\..\..\images\3_cherry.png");
+                        break;
+                    case 3:
+                        pictureBox2.Image = Image.FromFile(@"..\..\..\..\..\images\4_grapes.jpg");
+                        break;
+                }
+
+                switch (DisplayThree)
+                {
+                    case 1:
+                        pictureBox3.Image = Image.FromFile(@"..\..\..\..\..\images\2_apple.jpg");
+                        break;
+                    case 2:
+                        pictureBox3.Image = Image.FromFile(@"..\..\..\..\..\images\3_cherry.png");
+                        break;
+                    case 3:
+                        pictureBox3.Image = Image.FromFile(@"..\..\..\..\..\images\4_grapes.jpg");
+                        break;
+                }
+            }
+            else
+            {
+                timer1.Stop();
+                button1.Visible = true;
+                button2.Visible = false;
+                Counter = 30;
+
+                if (DisplayOne == DisplayTwo && DisplayOne == DisplayThree)
+                {
+                    label1.Text = "JackPot! Освоивте 1.000.000 денари!";
+                    pictureBox4.Image = Image.FromFile(@"..\..\..\..\..\images\coins.png");
+                }
+                else
+                {
+                    label1.Text = "Немавте среќа, обидете се повторно!";
+                }
+            }
+        }
+    }
+}
